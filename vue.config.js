ae8876df92d9bad6c48a9d69d5430ee2c6f66a8e
Prommin Intani <prommin.i@zealtechinter.com>
2022-09-19 19:28:09 +0700

    change env

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   publicPath:
-    process.env.NODE_ENV === "production" ? "/https://cms-vue-dream1411.vercel.app/" : "/",
+    process.env.NODE_ENV === "production" ? "/cms-vue-dream1411.vercel.app/" : "/",
   transpileDependencies: true,
   chainWebpack: (config) => {
     config.resolve.alias.set("vue-i18n", "vue-i18n/dist/vue-i18n.cjs.js");
